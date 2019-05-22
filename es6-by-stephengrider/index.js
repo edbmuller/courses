ac4c80846fc988cc9dd54d1ddf9c33db9852759c
Eduardo Müller <edbmuller@gmail.com>
2019-05-21 22:42:26 -0300

    es6 - first lesson

---
+++ es6-by-stephengrider/index.js
@@ -0,0 +1,24 @@
+
+// array helpers
+
+const colors = ['blue', 'gray', 'black', 'red'];
+
+// traditional for
+// for( var i = 0; i < colors.length; i++ ) {
+//   console.log(colors[i]);
+// }
+
+// foreach
+
+colors.forEach( color => {
+  console.log( color );
+});
+
+
+// map();
+// filter();
+// find();
+// every();
+// some();
+// reduce();
+
