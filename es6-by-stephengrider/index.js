4046fb7f6ce50b5eaffe311840a979e6574c69ed
Eduardo Müller <edbmuller@gmail.com>
2019-05-24 00:40:28 -0300

    forEach study

--- es6-by-stephengrider/index.js
+++ es6-by-stephengrider/index.js
@@ -1,7 +1,7 @@
 
 // array helpers
 
-const colors = ['blue', 'gray', 'black', 'red'];
+const colors = ['blue', 'gray', 'black', 'red', ''];
 
 // traditional for
 // for( var i = 0; i < colors.length; i++ ) {
@@ -9,11 +9,19 @@ const colors = ['blue', 'gray', 'black', 'red'];
 // }
 
 // foreach
-
 colors.forEach( color => {
   console.log( color );
 });
 
+colors.forEach( ( value, index, colors ) => {
+  console.log(`Valor: ${ value }, index: ${ index }, array: ${ colors }`);
+});
+
+const logArrayElements = ( value, index, colors ) => {
+  console.log( `arr[${ index }] = ${ value }` );
+}
+
+colors.forEach( logArrayElements );
 
 // map();
 // filter();
