671d10c52dbdb46de145e5a9c74e7cc7accc3a11
Eduardo Müller <edbmuller@gmail.com>
2019-05-27 23:00:37 -0300

    forEach exercize

--- es6-by-stephengrider/index.js
+++ es6-by-stephengrider/index.js
@@ -17,11 +17,30 @@ colors.forEach( ( value, index, colors ) => {
   console.log(`Valor: ${ value }, index: ${ index }, array: ${ colors }`);
 });
 
+
 const logArrayElements = ( value, index, colors ) => {
   console.log( `arr[${ index }] = ${ value }` );
 }
-
 colors.forEach( logArrayElements );
+
+
+// Exercize: the images array contains objects with the sizes, calculate the area of each image and sote in a new array called areas.
+
+const images = [
+  {height: 10, width: 30},
+  {height: 20, width: 25},
+  {height: 40, width: 40}
+]
+const areas = [];
+
+images.forEach( (image) => {
+  areas.push( image.width * image.height );
+});
+
+console.log(areas);
+
+
+
 
 // map();
 // filter();
