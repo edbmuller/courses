b8a774b74565cb5e901ce581021be0583ab4c00e
Eduardo Müller <edbmuller@gmail.com>
2019-05-28 21:21:01 -0300

    map function: Exercizes

--- es6-by-stephengrider/map.js
+++ es6-by-stephengrider/map.js
@@ -26,16 +26,38 @@ let prices = cars.map( (car) => {
 
 console.log(prices);
 
-// Exercize: Using map, create a new array that contains the 'height' property of each Object.
 
+// Exercize: Using map, create a new array that contains the 'height' property of each Object.
 let images = [
   { height: '34px', width: '39px' },
   { height: '54px', width: '19px' },
   { height: '83px', width: '75px' },
 ];
 
-let heights = images.map( (image) => {
+let heights = images.map( image => {
   return image.height;
 });
 
 console.log(heights);
+
+// Exercize: create a new array that contains the speed value from each trip.
+var trips = [
+  { distance: 34, time: 10 },
+  { distance: 90, time: 50 },
+  { distance: 59, time: 25 }
+];
+
+let speeds = trips.map( trip => {
+  return trip.distance / trip.time;
+});
+
+console.log(speeds);
+
+// Exercize: Implement a 'plunk' function. Pluck should accept an array and a string representing a property name and return an array containing that property from each object.
+const pluck = ( array, property ) => {
+  return array.map( item => item[property] );
+}
+
+console.log( pluck( images, 'width' ) );
+console.log( pluck( trips, 'distance' ) );
+
